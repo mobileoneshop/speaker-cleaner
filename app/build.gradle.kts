42581ded759b3f2e1fcc5c55a465b754f16bcddd
umar <umar@local>
2026-09-25 19:44:55 +0500

    chore: change package applicationId to com.one.speakercleaner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.normathi.soniclean"
+    applicationId = "com.one.speakercleaner"
     minSdk = 21
     targetSdk = 36
     versionCode = 1
